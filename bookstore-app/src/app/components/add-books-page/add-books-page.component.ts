80070511193be9e082d6e996608a756085411eaa
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2020-10-03 12:23:47 +0300

    Add book form backbone

--- bookstore-app/src/app/components/add-books-page/add-books-page.component.ts
+++ bookstore-app/src/app/components/add-books-page/add-books-page.component.ts
@@ -19,18 +19,35 @@ export class AddBooksPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.addForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(120)]],
-      userEmail: ['', [Validators.required, Validators.email]],
-      description: ['', [Validators.required]],
-      categories: [[], [Validators.required]],
-      author: ['', [Validators.required]],
-      publisher: ['', [Validators.required]],
-      published: ['', [Validators.required]],
-      pages: [null, [Validators.required]],
-      rating: ['', []],
-      isbn10: ['', [Validators.required]],
-      isbn: ['', [Validators.required]],
+      title: ['', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(120),
+        Validators.pattern('^[a-zA-Z0-9@"#&*! ]+$')]    // regex for only allowing alphanumeric characters, space, and @"#&*!
+      ],
+      description: ['', [
+        Validators.required,
+        Validators.maxLength(512),
+        Validators.pattern('^[A-Z].*')]     // regex for starting with an uppercase letter
+      ],
+      categories: [[], [Validators.required, Validators.maxLength(4)]],
+      author: ['', [Validators.required]],    // TODO: list
+      publisher: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ]],
+      published: [null, [Validators.required]],
+      pages: [null, [Validators.required, Validators.min(0), Validators.max(9999)]],
+      rating: ['', [Validators.min(0), Validators.max(5)]],
+      isbn10: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      isbn: ['', [Validators.required, Validators.pattern('^[0-9]{13}$')]],
     });
+  }
+
+  isFormValid(): boolean {
+    console.log(this.addForm.valid);
+    return this.addForm.valid;
   }
 
   onImageUpload(imgBase64: string) {
@@ -40,8 +57,11 @@ export class AddBooksPageComponent implements OnInit {
   submitForm(): void {
     // TODO: construct IBook
     // this.bookService.postBook(newBook);
+    console.log(this.addForm.valid);
+    console.log(this.addForm);
     for (const i in this.addForm.controls) {
       this.addForm.controls[i].markAsDirty();
+      this.addForm.controls[i].markAsTouched();
       this.addForm.controls[i].updateValueAndValidity();
     }
     console.log(this.addForm.controls)
